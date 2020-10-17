b9a3dc9c3c6d816564ab3597953ad3e25b40dcc7
axeliono <69375446+axeliono@users.noreply.github.com>
2020-10-17 18:59:19 -0500

    added comments to explain thought process in javascript file

--- assets/js/csquiz.js
+++ assets/js/csquiz.js
@@ -3,8 +3,8 @@ var startBtn = document.getElementById("start");
 var questionEl = document.getElementById("question-item");
 var questionCounter = 0
 var playerScore = 0;
-//question objects
 
+//array that, at each index (position), holds an object that holds the information for a question
 var questionObjectArray = [{
     name: "question insert 1",
     answers: ["A. right","B. wrong","C. wrong","D. wrong"], 
@@ -32,19 +32,30 @@ var questionObjectArray = [{
     key: 0
 }]
 
-
+//function to start quiz once the start button is clicked
 var startQuiz = function() {
+    //hides start button 
     document.getElementById("start").style.visibility = "hidden";
-    
+
+    //function for timer
     countdown();
+
+    //function to create questions and answer choices on screen 
     runQuestions();
 }
 
 var countdown = function() {
+    //amount of seconds given
     var timeRemaining = 75;
+
+    //reset player score every time game starts
     playerScore = 0;
+
+    //variable to hold current question on screen
     questionCounter = 0;
-    
+
+
+    //actual timer 
     var timeInterval = setInterval(function() {
 
         if(timeRemaining > 0) {
@@ -53,8 +64,9 @@ var countdown = function() {
             
         }
         else {
+            //stops timer
             clearInterval(timeInterval);
-            //put function to send info for highscore
+            //endGame();
         }
 
         
@@ -65,20 +77,25 @@ var countdown = function() {
     
 }
 
-
+//function to put questions on screen
  var runQuestions = function() {
-     let questionHolder = document.getElementById("question-item");
+
+    //create element to hold question then create 
+     let questionHolderEl = document.getElementById("question-item");
      let questionShown = document.createElement("div");
-     let answerHolder = document.getElementById("answer-choices-shown");
+
+     //create element to hold answer choice and create unordered list to prepare to add answer choice list elements
+     let answerHolderEl = document.getElementById("answer-choices-shown");
      let answersShown = document.createElement("ul");
 
-     answerHolder.appendChild(answersShown);
+     //append holder for answers we create to the div element 'answer-choices-shown' that is by default in the html file
+     answerHolderEl.appendChild(answersShown);
      currentQuestion = questionObjectArray[questionCounter];
 
      //grab question info and place object.name into div element
      currentQName = currentQuestion.name;
      questionShown.innerHTML = "<h3> " + currentQName + "</h3>";
-     questionHolder.appendChild(questionShown);
+     questionHolderEl.appendChild(questionShown);
 
      //add the answer choices from the array of objects
      for(i = 0; i <currentQuestion.answers.length; i++) {
@@ -102,20 +119,48 @@ var countdown = function() {
 
  }
 
+//function to move to next question when click event occurs on answer choice list element
  var nextQuestion = function (event) {
-     questionCounter++;
-     if(event.target.getAttribute("correct-answer") === "true") {
-         playerScore += 5;
+     //if question counter has not reached the maximum number then move to next question
+     if(questionCounter < 4) {
+
+        //element holding child elements involving all question info.
+        let questionDataEl = document.getElementById("question-item");
+        //element holding child elements involving all answer choice info.
+        let answerDataEl = document.getElementById("answer-choices-shown");
+        //increase question counter
+        questionCounter++;
+        //add to player score if correct answer is clicked
+        if(event.target.getAttribute("correct-answer") === "true") {
+            playerScore += 5;
+        }
+        //make the elements holding question/answer info. blank so that the runquestions function can add new elements with the new question's information
+        questionDataEl.innerHTML = '';
+        answerDataEl.innerHTML = '';
+        runQuestions();
      }
-     runQuestions();
+
+     //go to endgame function to end the game 
+     else {
+         endGame();
+     }
 
  }
-
+//function to end the game by blanking page content (main tag with page-content class in html)
  var endGame = function() {
      //clear screen
-    document.getElementById("question-item").children.clear();
-    document.getElementById("answer-choices-shown").children.clear();
+    let theScreen = document.getElementsByClassName("page-content");
+    theScreen.innerHTML = '';
  }
 
+ //function to create elements for the player's score and other info.
+ var finalScoreInfo = function() {
+     let finalScoreEl = document.createElement("li")
+
+     finalScoreEl.textContent = playerScore;
+
+ }
+
+ //event listener for clicking start button
 startBtn.addEventListener("click", startQuiz);
 
